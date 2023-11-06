00ef4ad1f642bb00e27934a690570f5fde2b708a
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-07 00:43:45 +0100

    Initial commit

---
+++ Exercises/Exercise9.cs
@@ -0,0 +1,19 @@
+namespace Exercises
+{
+    internal class Exercise9
+    {
+        public static void Execute()
+        {
+            double number1, number2, number3;
+            
+            Console.WriteLine("Enter the first number: ");
+            number1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            number2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the third number: ");
+            number3 = double.Parse(Console.ReadLine());
+            
+            //To be continued.
+        }
+    }
+}
