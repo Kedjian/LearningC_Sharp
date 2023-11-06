c816ec7f5ff5bd352190ef55f486128680a83baf
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-06 21:10:54 +0100

    Initial Commit

---
+++ Exercises/Exercise1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LearningBox
+{
+    internal class Exercise1
+    {
+        static void Main(string[] args)
+        {
+            string text;
+
+            Console.WriteLine("Enter your name: ");
+            text = Console.ReadLine();
+            text = "Hello " + text + "!";
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
