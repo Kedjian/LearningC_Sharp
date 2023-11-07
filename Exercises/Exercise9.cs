861757445849fada2830dc4e9d57b325a6a2a33d
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-07 07:34:10 +0100

    Second commit

--- Exercises/Exercise9.cs
+++ Exercises/Exercise9.cs
@@ -4,16 +4,47 @@ namespace Exercises
     {
         public static void Execute()
         {
-            double number1, number2, number3;
+            double a, b, c;
             
             Console.WriteLine("Enter the first number: ");
-            number1 = double.Parse(Console.ReadLine());
+            a = double.Parse(Console.ReadLine());
             Console.WriteLine("Enter the second number: ");
-            number2 = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
             Console.WriteLine("Enter the third number: ");
-            number3 = double.Parse(Console.ReadLine());
-            
-            //To be continued.
+            c = double.Parse(Console.ReadLine());
+
+            if (a == b | a == c | b == c)
+            {
+                Console.WriteLine("The numbers are no different from each other.");
+            }
+            else
+            {
+                if (a > c && a < b)
+                {
+                    Console.WriteLine("The middle number is: " + a);
+                }
+
+                if (a < c && a > b)
+                {
+                    Console.WriteLine("The middle number is: " + a);
+                }
+                if (b > a && b < c)
+                {
+                    Console.WriteLine("The middle number is: " + b);
+                }
+                if (b < a && b > c)
+                {
+                    Console.WriteLine("The middle number is: " + b);
+                }
+                if (c > a && c < b)
+                {
+                    Console.WriteLine("The middle number is: " + c);
+                }
+                if (c < a && c > b)
+                {
+                    Console.WriteLine("The middle number is: " + c);
+                }
+            }
         }
     }
 }
