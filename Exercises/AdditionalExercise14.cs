95c9dd165d5072d17da8af91236c36dac4ebfe41
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-11 12:04:56 +0100

    Initial commit

--- Exercises/AdditionalExercise14.cs
+++ Exercises/AdditionalExercise14.cs
@@ -17,12 +17,12 @@ namespace Exercises
                 Console.WriteLine("You cannot divide by zero.");
             }
 
-            if (num1 > 0 && num % num1 == 0)
+            else if (num % num1 == 0)
             {
                 Console.WriteLine("{0} is a divider of {1}.", num1, num);
                 Console.ReadKey();
             }
-            else if (num1 > 0)
+            else
             {
                 Console.WriteLine("{0} is not a divider of {1}.", num1, num);
                 Console.ReadKey();
