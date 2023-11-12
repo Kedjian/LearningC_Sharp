25dab7b383fd8f8e77de12524179cd73b157b171
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-12 21:33:14 +0100

    Initial commit

---
+++ Exercises/RootsEnumeratorAlternative.cs
@@ -0,0 +1,51 @@
+namespace Exercises
+{
+    internal class RootsEnumeratorAlternative
+    {
+        public static void Execute()
+        {
+            double delta;
+            double x0;
+            double x1;
+            double x2;
+
+            double a;
+            Console.WriteLine("Enter the A integer: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            double b;
+            Console.WriteLine("Enter the B integer: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            double c;
+            Console.WriteLine("Enter the C integer: ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            if (a != 0)
+            {
+                delta = Math.Pow(b, 2) - 4 * a * c;
+
+                if (delta > 0)
+                {
+                    x1 = -b - Math.Sqrt(delta) / 2 * a;
+                    x2 = -b + Math.Sqrt(delta) / 2 * a;
+                    Console.WriteLine("Delta has two square roots. x1 equals {0}, and x2 equals {1}.", x1, x2);
+                    Console.ReadKey();
+                } else if (delta == 0)
+                {
+                    x0 = -b / 2 * a;
+                    Console.WriteLine("Delta has one square root, and it equals {0}.", x0);
+                    Console.ReadKey();
+                } else
+                {
+                    Console.WriteLine("Delta has no square roots.");
+                    Console.ReadKey();
+                }
+            } else
+            {
+                Console.WriteLine("A cannot be a negative integer. Try again.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
