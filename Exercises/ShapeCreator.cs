b4772f8635ac8d45cd9a1050dc4ee1fb69c01d6b
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-16 22:20:17 +0100

    Case 1 fixed

--- Exercises/ShapeCreator.cs
+++ Exercises/ShapeCreator.cs
@@ -9,25 +9,24 @@ namespace Exercises
             
             Console.WriteLine("Enter 1 for a triangle, 2 for its horizontally swapped version, 3 for its inverted version, and 4 for a 0 figure.");
             choice = Convert.ToChar(Console.ReadLine());
-            Console.WriteLine("");
-
+            
             switch (choice)
             {
                 case '1':
-                    for (int i = 5; i != 0; --i)
+                    for (int i = 1; i < 5; i++)
                     { 
                         Console.Write("\n");
-                        for (int n = 5; n != i; --n)
+                        for (int j = 0; j < i; j++)
                         {
                             Console.Write(sym);
                         }
                     }
                     break;
                 case '2':
-                    for (int i = 0; i != 4; ++i)
+                    for (int i = 0; i != 4; i++)
                     { 
                         Console.Write("\n");
-                        for (int n = 4; n != i; --n)
+                        for (int j = 4; j != i; j--)
                         {
                             Console.Write(sym);
                         }
