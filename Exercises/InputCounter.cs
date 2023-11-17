75c60f6b98e89388cac6dff3c4d431c95e45dd36
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-17 23:56:31 +0100

    Second commit, still needs a fix.

--- Exercises/InputCounter.cs
+++ Exercises/InputCounter.cs
@@ -4,23 +4,21 @@ namespace Exercises
     {
         public static void Execute()
         {
-            int num = 0;
-            int temp;
-            int numSum = 0;
+            int digit;
+            int sum = 0;
+            int numSum = 1;
 
             do
             {
                 Console.WriteLine("Enter a digit: ");
-                temp = Convert.ToInt32(Console.ReadLine());
+                digit = Convert.ToInt32(Console.ReadLine());
                 
-                temp = num;
-                num += num;
-                numSum++;
+                digit += sum;
                 
                 // Doesn't work, it needs a fix.
                 
-                Console.Write("{0} {1} {2}", temp, num, numSum);
-            } while (temp == 0);
+                Console.WriteLine("{0} {1}", digit, numSum);
+            } while (digit == 0);
             Console.WriteLine("{0} was the amount of digits entered.", numSum);
         }
     }
