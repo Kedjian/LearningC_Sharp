a54aa1e12ae53ad5df2dbf117c59c20fef335c79
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-19 19:36:26 +0100

    Internal translation

--- Exercises/BoolPostPreIncrementationExample04.cs
+++ Exercises/BoolPostPreIncrementationExample04.cs
@@ -5,9 +5,9 @@ namespace Exercises
         public static void Execute()
         {
             int x = 1, y = 3, z = 4;
-            bool wynik = (x == 1 || y++ > 2 || ++z > 0);
-            Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
-            //wynik = true, x = 1, y = 3, z = 4
+            bool result = (x == 1 || y++ > 2 || ++z > 0);
+            Console.WriteLine("Result={0} x={1} y={2} z={3}", result, x, y, z);
+            //Result = true, x = 1, y = 3, z = 4
         }
     }
 }
