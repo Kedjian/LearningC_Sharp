882b8fe6f33cbfae4a3dab0b14b2bbded4e38523
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-22 22:25:56 +0100

    Initial commit

--- Exercises/Application.cs
+++ Exercises/Application.cs
@@ -6,7 +6,7 @@ namespace Exercises
     {
         static void Main(string[] args)
         {
-            // NestedLoopShapeCreationExample03.Execute();
+            PrimeNumberEnumenator.Execute();
         }
     }
 }
