84486ae3945ec31c2d1780d5d75a0de301597e90
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-24 23:39:59 +0100

    Initial commit, but I don't understand a single line

---
+++ Exercises/TwinPrimesEnumenator.cs
@@ -0,0 +1,37 @@
+namespace Exercises
+{
+    internal class TwinPrimesEnumenator
+    {
+        public static void Execute()
+        {
+            for (int i = 2; i <= 100; i++)
+            {
+                bool isPrime1 = true;
+                bool isPrime2 = true;
+
+                for (int j = 2; j <= Math.Sqrt(i) || j <= Math.Sqrt(i + 2); j++)
+                {
+                    if (i % j == 0 && j <= Math.Sqrt(i))
+                    {
+                        isPrime1 = false;
+                    }
+
+                    if ((i + 2) % j == 0 && j <= Math.Sqrt(i + 2))
+                    {
+                        isPrime2 = false;
+                    }
+
+                    if (!isPrime1 && !isPrime2)
+                    {
+                        break;
+                    }
+                }
+
+                if (isPrime1 && isPrime2)
+                {
+                    Console.WriteLine($"({i}, {i + 2}");
+                }
+            }
+        }
+    }
+}
