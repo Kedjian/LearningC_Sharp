c390a963728c2f7a7299206c8a384129ec907ab6
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-25 23:33:03 +0100

    Added RangeChecker

--- Exercises/Application.cs
+++ Exercises/Application.cs
@@ -6,7 +6,7 @@ namespace Exercises
     {
         static void Main(string[] args)
         { 
-            MultiplicationTableEnumenator.Execute();
+            RangeChecker.Execute();
         }
     }
 }
