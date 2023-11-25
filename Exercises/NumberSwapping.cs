9dadfa0d34e311314af420bf418a258a2a721aa5
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-25 23:11:22 +0100

    Initial commit

---
+++ Exercises/NumberSwapping.cs
@@ -0,0 +1,24 @@
+namespace Exercises
+{
+    internal class NumberSwapping
+    {
+        public static void Execute()
+        {
+            int num1 = 6;
+            int num2 = 10;
+            int temp;
+            
+            Console.WriteLine("Integer called 'num1' has a value of {0}, while integer called 'num2' has a value of {1}.", num1, num2);
+            Console.WriteLine("Press any key to swap their values.");
+            Console.ReadKey();
+
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            
+            Console.WriteLine("Integer called 'num1' changed its value to {0}, and so did 'num2' which is now equal to {1}.", num1, num2);
+            Console.WriteLine("Press any key to end the program.");
+            Console.ReadKey();
+        }
+    }
+}
