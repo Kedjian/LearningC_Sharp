5cd33356e8522c98585d7621c83b7062780ea60b
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-25 23:21:01 +0100

    Initial commit

---
+++ Exercises/MultiplicationTableEnumenator.cs
@@ -0,0 +1,25 @@
+namespace Exercises
+{
+    internal class MultiplicationTableEnumenator
+    {
+        public static void Execute()
+        {
+            Console.WriteLine("Enter a digit that's lower than 10.");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num < 10 & num > 0)
+            {
+                for (int i = 1; i < 10; i++)
+                {
+                    int temp = num;
+                    temp *= i;
+                    Console.WriteLine(num + "*" + i + "=" + temp);
+                }
+            }
+            else
+            {
+                Console.WriteLine("You entered a wrong digit.");
+            }
+        }
+    }
+}
