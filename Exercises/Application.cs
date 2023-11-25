e3d4c64fc07330cc21ddf2799b483d660da52f22
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-25 23:21:20 +0100

    Added MultiplicationTableEnumenator

--- Exercises/Application.cs
+++ Exercises/Application.cs
@@ -6,7 +6,7 @@ namespace Exercises
     {
         static void Main(string[] args)
         { 
-            NumberSwapping.Execute();
+            MultiplicationTableEnumenator.Execute();
         }
     }
 }
