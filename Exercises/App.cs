37f319c3fb356fe84ccf4052cefc3f5c8fe19bfd
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-26 16:30:32 +0100

    Clearer naming convention, moved to Exercises directory

--- Exercises/App.cs
+++ Exercises/App.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         { 
-            // Exercises.conditions.ConditionsExercise13.Execute();
+            Exercises.conditions.ConditionsExercise13.Execute();
         }
     }
 }
