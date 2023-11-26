d14bbfbbd9f965ffc2dfe481c1b0fa539a1d8b51
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-26 16:21:41 +0100

    Clearer name convention

---
+++ Exercises/App.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercises
+{
+    internal class App
+    {
+        static void Main(string[] args)
+        { 
+            // Exercises.conditions.ConditionsExercise13.Execute();
+        }
+    }
+}
