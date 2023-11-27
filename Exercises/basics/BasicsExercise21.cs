002df8a2528694a0b2872da0532f0200e9cbe180
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-27 23:02:57 +0100

    Initial commit

---
+++ Exercises/basics/BasicsExercise21.cs
@@ -0,0 +1,21 @@
+namespace Exercises.basics
+{
+    internal class BasicsExercise21
+    {
+        public static void Execute()
+        {
+            Console.WriteLine(5 + 2);
+            // expected output: 7
+            Console.WriteLine(10 / 2);
+            // expected output: 5
+            Console.WriteLine(-1 + (4 * 6));
+            // expected output: 23
+            Console.WriteLine((35 + 5) % 7);
+            // expected output: 5
+            Console.WriteLine(14 + (-4 * 6 / 11));
+            // expected output: 12
+            Console.WriteLine(2 + (15 / 6 * 1) - 7 % 2);
+            // expected output: 3
+        }
+    }
+}
