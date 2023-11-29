f84c657d47f3b85f10acbbf0b26c0483387c3199
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-29 20:44:34 +0100

    Added BasicsExercise23

--- Exercises/App.cs
+++ Exercises/App.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         { 
-            Exercises.loops.LoopsExercise22.Execute();
+            Exercises.basics.BasicsExercise23.Execute();
         }
     }
 }
