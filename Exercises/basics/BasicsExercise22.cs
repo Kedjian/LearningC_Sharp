d009bededfbdd5bfab3dedc0cbc6d82775011854
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-29 20:40:16 +0100

    Initial commit

--- Exercises/basics/BasicsExercise22.cs
+++ Exercises/basics/BasicsExercise22.cs
@@ -4,15 +4,15 @@ namespace Exercises.basics
     {
         public static void Execute()
         {
-            int n1, n2, n3, res;
-            Console.WriteLine("Enter a first digit: ");
-            n1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter a second digit: ");
-            n2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter a third digit: ");
-            n3 = Convert.ToInt32(Console.ReadLine());
-            res = n1 * n2 * n3;
-            Console.WriteLine("{0} * {1} * {2} = {3}.", n1, n2, n3, res);
+            Console.WriteLine(remove_char("w3resource", 1)); // removes character at index 1
+            Console.WriteLine(remove_char("w3resource", 9)); // removes character at index 9 (if exists)
+            Console.WriteLine(remove_char("w3resource", 0)); // removes character at index 0
+        }
+
+        // function to remove a character at the specified index
+        public static string remove_char(string str, int n)
+        {
+            return str.Remove(n, 1); // using remove method to eliminate the character at index n
         }
     }
 }
