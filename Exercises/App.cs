486803a677fa877b990e03d9c1c9ee9a1e3ae5af
Oskar Makowski <o.mat.makowski@gmail.com>
2023-11-29 21:37:13 +0100

    Added ConditionsExercise24

--- Exercises/App.cs
+++ Exercises/App.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         { 
-            Exercises.conditions.ConditionsExercise23.Execute();
+            Exercises.conditions.ConditionsExercise24.Execute();
         }
     }
 }
