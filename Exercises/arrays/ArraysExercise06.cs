a3eee8517759c633abf10576ee707cc9a09afe61
Oskar Makowski <o.mat.makowski@gmail.com>
2023-12-04 23:25:30 +0100

    Loop iteration fix

--- Exercises/arrays/ArraysExercise06.cs
+++ Exercises/arrays/ArraysExercise06.cs
@@ -4,18 +4,18 @@ namespace Exercises.arrays
     {
         public static void Execute()
         {
-            int[] arr = new int[10];
+            int[] arr = new int[11];
             int i;
             Console.Write("\n\nRead and print elements of an array:\n");
             Console.Write("------------------------------------\n");	
             
-            for (i=1; i<10; i++)
+            for (i=1; i<=10; i++)
             {
                 Console.Write("Element {0}: ", i);
                 arr[i] = Convert.ToInt32(Console.ReadLine());
             }
             Console.Write("\nElements in the array are: ");  
-            for(i=0; i<10; i++)  
+            for(i=0; i<=10; i++)  
             {
                 if (i < 9)
                 { 
