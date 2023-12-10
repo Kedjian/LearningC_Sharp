bd8128d848df97add3ed8b53e1d25cc240863765
Oskar Makowski <o.mat.makowski@gmail.com>
2023-12-10 14:59:11 +0100

    Initial commit

---
+++ Exercises/arrays/ArraysExercise14.cs
@@ -0,0 +1,14 @@
+namespace Exercises.arrays
+{
+    internal class ArraysExercise14
+    {
+        public static void Execute()
+        {
+            string text = "w3resource.com";
+            foreach (char i in text)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
