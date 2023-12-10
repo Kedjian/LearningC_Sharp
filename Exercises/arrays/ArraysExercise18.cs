29f0279b9845f36e97b98ba6c6d11796b98a9023
Oskar Makowski <o.mat.makowski@gmail.com>
2023-12-10 23:16:54 +0100

    Clarity fix

--- Exercises/arrays/ArraysExercise18.cs
+++ Exercises/arrays/ArraysExercise18.cs
@@ -7,49 +7,36 @@ namespace Exercises.arrays
             
             int size;
             double sum = 0;
-            double arithmetic = 0;
             int positives = 0;
             do
             {
-                Console.WriteLine("\nEnter the size of an array: ");
+                Console.WriteLine("Enter the size of an array (should be greater than 1): ");
                 size = Convert.ToInt32(Console.ReadLine());
                 if (size <= 1)
                 {
-                    Console.WriteLine("\nYou cannot pick negative integers, and 1 or 0.");
+                    Console.WriteLine("Array size should be greater than 1. Please try again.");
                 }
             }
             while (size <= 1);
             int[] arr1 = new int[size];
+
             for (int iter = 0; iter < size; iter++)
             {
                 Console.Write("Enter element {0}: ", iter);
                 arr1[iter] = Convert.ToInt32(Console.ReadLine());
-            }
-            Array.Sort(arr1);
-            Console.WriteLine("\nThe array got sorted, so integer positions changed! Have that in mind please.");
-            /* It's definitely easier to 
-             sort the array instead of 
-             doing mental gymnastics. */
-            Console.WriteLine("The smallest integer lies now in the position of {0} and is equal to {1}.", size-size, arr1[0]);
-            /* Prints the smallest integer
-             and its position as well */
-            Console.WriteLine("The biggest integer lies now in the position of {0} and is equal to {1}.", size-1, arr1[size-1]);
-            /* Prints the biggest integer
-             and its position as well*/
-            foreach (int num in arr1)
-            {
-                sum += num;
-                arithmetic = sum / size;
-            }
-            Console.WriteLine("The arithmetic average of the array equals to: {0}.", arithmetic);
-            foreach (int numCheck in arr1)
-            {
-                if (numCheck > 0)
+                sum += arr1[iter];
+
+                if (arr1[iter] > 0)
                 {
                     positives++;
                 }
             }
-            Console.WriteLine("The number of positive integers in the array equal to: {0}.", positives);
+            Array.Sort(arr1);
+            Console.WriteLine("\nThe array has been sorted in ascending order.");
+            Console.WriteLine("The smallest integer is: {0} at position 0.", arr1[0]);
+            Console.WriteLine("The biggest integer is: {0} at position {1}.", arr1[size - 1], size - 1);
+            Console.WriteLine("The arithmetic average of the array is: {0}.", sum / size);
+            Console.WriteLine("The number of positive integers in the array is: {0}.", positives);
         }
     }
 }
