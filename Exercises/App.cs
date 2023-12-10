da6f7bdbd6abab111435a92605f016d3386582cf
Oskar Makowski <o.mat.makowski@gmail.com>
2023-12-10 15:05:39 +0100

    Added ArraysExercise15

--- Exercises/App.cs
+++ Exercises/App.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         { 
-            Exercises.arrays.ArraysExercise14.Execute();
+            Exercises.arrays.ArraysExercise15.Execute();
         }
     }
 }
