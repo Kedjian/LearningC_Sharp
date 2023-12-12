51e10da2d268052a1f7163a5fc54538f1deba5f9
Oskar Makowski <o.mat.makowski@gmail.com>
2023-12-12 17:20:28 +0100

    Initial commit

---
+++ Exercises/arrays/ArraysExercise20.cs
@@ -0,0 +1,17 @@
+namespace Exercises.arrays
+{
+    internal class ArraysExercise20
+    {
+        public static void Execute()
+        {
+            int[] arr1 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] arr2 = new int[10];
+            Array.Copy(arr1,0,arr2,1,9);
+            Array.Copy(arr1,9,arr2,0,1);
+            foreach (int num in arr2)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
