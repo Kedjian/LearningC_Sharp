d55ad03c45dbb320045b6c1412eb96c6b4e1550a
Oskar Makowski <o.mat.makowski@gmail.com>
2023-12-19 16:44:51 +0100

    Added MethodsExercise03

--- Exercises/App.cs
+++ Exercises/App.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         { 
-            Exercises.methods.MethodsExercise02.Execute(); 
+            Exercises.methods.MethodsExercise03.Execute(); 
         }
     }
 }
